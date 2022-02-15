32cb07b011644f61e42736d281784d5235daa5f0
SIYUAN XU <xsiyuan@gmail.com>
2022-02-15 14:04:08 +0800

    ribbon 负载均衡

---
+++ ribbon/ribbon-consumer/src/main/java/com/imooc/ribbon/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.imooc.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/** @author afu */
+@RestController
+public class Controller {
+  @Autowired private RestTemplate restTemplate;
+
+  @GetMapping("/sayHi")
+  public String sayHi() {
+    return restTemplate.getForObject("http://eureka-client/sayHi", String.class);
+  }
+}
