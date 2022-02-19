10141171484a06ca3cde1d2972c4f9826a189b3b
SIYUAN XU <xsiyuan@gmail.com>
2022-02-19 10:41:56 +0800

    feign 服务界阔

---
+++ feign/feign-client-interface/src/main/java/com/imooc/Friend.java
@@ -0,0 +1,10 @@
+package com.imooc;
+
+import lombok.Data;
+
+/** @author afu */
+@Data
+public class Friend {
+  private String name;
+  private String port;
+}
---
+++ feign/feign-client-interface/src/main/java/com/imooc/IService.java
@@ -0,0 +1,16 @@
+package com.imooc;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/** @author afu */
+@FeignClient("feign-client")
+public interface IService {
+  @GetMapping("/sayHi")
+  String sayHi();
+
+  @PostMapping("/sayHi")
+  Friend sayHiPost(@RequestBody Friend friend);
+}
