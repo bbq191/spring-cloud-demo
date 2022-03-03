322b979ed4682ad36a6915456e7cdb6c2f201d1c
SIYUAN XU <xsiyuan@gmail.com>
2022-03-03 16:06:09 +0800

    dashboard 集成

--- hystrix/hystrix-dashboard/src/main/java/com/imooc/HystrixDashboardApplication.java
+++ hystrix/hystrix-dashboard/src/main/java/com/imooc/HystrixDashboardApplication.java
@@ -1,10 +1,19 @@
 package com.imooc;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 /**
  * @author afu
  */
+@EnableHystrixDashboard
+@SpringCloudApplication
 public class HystrixDashboardApplication {
   public static void main(String[] args) {
-    //
+    new SpringApplicationBuilder(HystrixDashboardApplication.class)
+        .web(WebApplicationType.SERVLET)
+        .run(args);
   }
 }
