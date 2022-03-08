0176cec5b19aa096aba9f098fca711014f16498a
SIYUAN XU <xsiyuan@gmail.com>
2022-03-08 13:47:40 +0800

    config-client 借助 eureka 实现高可用

--- config/config-server-eureka/src/main/java/com/imooc/ConfigServerEurekaApplication.java
+++ config/config-server-eureka/src/main/java/com/imooc/ConfigServerEurekaApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigClientApplication {
+public class ConfigServerEurekaApplication {
   public static void main(String[] args) {
-    new SpringApplicationBuilder(ConfigClientApplication.class)
+    new SpringApplicationBuilder(ConfigServerEurekaApplication.class)
         .web(WebApplicationType.SERVLET)
         .run(args);
   }
