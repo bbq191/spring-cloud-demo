b422114037d7bd21ba0fbeb139a75398c31d4232
SIYUAN XU <xsiyuan@gmail.com>
2022-03-27 10:10:21 +0800

    stream 自定义通道绑定

---
+++ stream/stream-sample/src/main/java/com/imooc/biz/MessageBean.java
@@ -0,0 +1,11 @@
+package com.imooc.biz;
+
+import lombok.Data;
+
+/**
+ * @author afu
+ */
+@Data
+public class MessageBean {
+  private String payload;
+}
--- stream/stream-sample/src/main/java/com/imooc/biz/StreamConsumer.java
+++ stream/stream-sample/src/main/java/com/imooc/biz/StreamConsumer.java
@@ -1,16 +1,23 @@
 package com.imooc.biz;
 
+import com.imooc.topic.MyTopic;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 
 @Slf4j
-@EnableBinding(value = {Sink.class})
+@EnableBinding(value = {Sink.class, MyTopic.class})
 public class StreamConsumer {
 
   @StreamListener(Sink.INPUT)
   public void consume(Object payload) {
     log.info("message consumed successfully, payload={}", payload);
+  }
+
+  // 自定义消息广播
+  @StreamListener(MyTopic.INPUT)
+  public void consumeMyMessage(Object payload) {
+    log.info("My message consumed successfully, payload={}", payload);
   }
 }
